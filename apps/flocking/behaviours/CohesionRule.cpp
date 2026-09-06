65002dc708b81079717fc4bfb6f6b32fda50da09
Joshua Mathieson <joshua.mathieson@mymail.champlain.edu>
2026-09-06 13:59:39 -0400

    Updated the cohesion rule logic. Removed assignment comments  from the cohesion rule as well.

--- apps/flocking/behaviours/CohesionRule.cpp
+++ apps/flocking/behaviours/CohesionRule.cpp
@@ -2,21 +2,18 @@
 #include <glm/glm.hpp>
 
 glm::vec2 CohesionRule::computeForce(const std::vector<BoidView>& neighborhood, const BoidView& boid) {
-  glm::vec2 cohesionForce(0.f);
-
-  // glm::length(vec) returns the length of a vector,
-  // glm::normalize(vec) returns the normalized vector (length 1) in the same direction as vec.
-
-  // begin solution
   if (!neighborhood.empty()) {
     glm::vec2 centerOfMass(0.f);
 
-    for (BoidView otherBoid : neighborhood)
-      centerOfMass += otherBoid.position;
-    
-    return centerOfMass / static_cast<float>(neighborhood.size()) - boid.position;
-  }
-  // end solution
+    for (BoidView otherBoid : neighborhood) centerOfMass += otherBoid.position;
 
-  return cohesionForce;
+    // Average the center of mass
+    centerOfMass /= neighborhood.size();
+
+    glm::vec2 difference = centerOfMass - boid.position;
+
+    return glm::normalize(difference) * glm::length(difference);
+  }
+
+  return glm::vec2(0.0f, 0.0f);
 }
